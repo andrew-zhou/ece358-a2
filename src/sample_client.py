f15414acc1bd587eaa2a698a1d7fc37974b0c745
Andrew Zhou <mrehee@gmail.com>
2017-07-22 14:17:19 -0400

    oops

--- src/sample_client.py
+++ src/sample_client.py
@@ -9,6 +9,7 @@ q = Queue()
 m = Manager('127.0.0.1', 5052, q)
 m.connections[('127.0.0.1', 5051)] = Connection('127.0.0.1', 5052, '127.0.0.1', 5051)
 t = Thread(target=m.start)
+t.daemon = True
 t.start()
 m.connections[('127.0.0.1', 5051)]._send_syn()
 while True:
--- src/sample_server.py
+++ src/sample_server.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     q = Queue()
     m = Manager('127.0.0.1', 5051, q)
     t = Thread(target=m.start)
+    t.daemon = True
     t.start()
     while True:
         c = q.get()
