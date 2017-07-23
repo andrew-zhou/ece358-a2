73883426fa366ffe420f46f67d85bcfab1f34e3e
GiliamMeeussen <giliam.m27@gmail.com>
2017-07-23 01:07:16 -0400

    clean client changes

--- src/client/cleanclient.py
+++ src/client/cleanclient.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import os
 import sys
+from datetime import datetime
 
 from queue import Queue
 from threading import Thread
@@ -29,6 +30,9 @@ class NiceClient(object):
         #       -> establishes connection and prints what it recieves
         # add <server_ip> <server_port>
         #       -> asks for some text to append to file, prints what it recieves
+        # dis <server_ip> <server_port>
+        #       -> initiates close
+        # 
         while True:
             inp = input('>>')
             cmd = inp.split(' ')
@@ -46,7 +50,8 @@ class NiceClient(object):
                 connections[(server_ip, server_port)] = conn_queue.get()
                 print('We got em')
                 data = application_recv(connections[(server_ip, server_port)])
-                print(data)
+                print('recieved')
+                # print(data)
 
             elif cmd[0] == 'add':
                 if cmd[1] == 'last':
@@ -59,7 +64,8 @@ class NiceClient(object):
                 application_send(connections[(server_ip, server_port)], bytes(text, 'utf-8'))
                 print('sent, now receiving: ')
                 data = application_recv(connections[(server_ip, server_port)])
-                print(data)
+                print('recieved')
+                # print(data)
 
             elif cmd[0] == 'dis':
                 if cmd[1] == 'last':
@@ -70,6 +76,40 @@ class NiceClient(object):
                     last = (server_ip, server_port)
                 connections[(server_ip, server_port)].close()
 
+            elif cmd[0] == 'file':
+                if cmd[1] == 'last':
+                    server_ip, server_port = last
+                else:
+                    server_ip = cmd[1]
+                    server_port = int(cmd[2])
+                    last = (server_ip, server_port)
+                file_name = input('Enter the filename: ')
+                file = open(file_name, 'rb')
+                file_data = file.read()
+
+                application_send(connections[(server_ip, server_port)], file_data)
+                print('sent, now receiving: ')
+                data = application_recv(connections[(server_ip, server_port)])
+                print('received')
+                print(data)
+
+            elif cmd[0] == 'time':
+                if cmd[1] == 'last':
+                    server_ip, server_port = last
+                else:
+                    server_ip = cmd[1]
+                    server_port = int(cmd[2])
+                    last = (server_ip, server_port)
+                file_name = input('Enter the filename: ')
+                file = open(file_name, 'rb')
+                file_data = file.read()
+
+                start = datetime.now()
+                application_send(connections[(server_ip, server_port)], file_data)
+                data = application_recv(connections[(server_ip, server_port)])
+                end = datetime.now()
+                print (end - start)
+
             else:
                 print('Sorry no comprende')
 
@@ -79,4 +119,3 @@ if __name__ == '__main__':
     ip = get_non_loopback_ip()
     client = NiceClient(ip, int(port))
     client.start()
-    
