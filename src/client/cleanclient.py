9ae3f83649f1a1b96e65c41ab65cc4f6e7f0ceda
GiliamMeeussen <giliam.m27@gmail.com>
2017-07-22 23:58:46 -0400

    Made a client that can con, add, and dis

---
+++ src/client/cleanclient.py
@@ -0,0 +1,82 @@
+#!/bin/python3
+import os
+import sys
+
+from queue import Queue
+from threading import Thread
+from rdt.manager import Manager
+from rdt.connection import Connection
+from rdt.connection import ConnectionClosedException, ConnectionStatus
+
+from util import *
+
+class NiceClient(object):
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def start(self):
+        conn_queue = Queue()
+        connections = {}
+        m = Manager(self.ip, self.port, conn_queue)
+        t = Thread(target=m.start)
+        t.daemon = True
+        t.start()
+
+        # USAGE:
+        # can use 'last', instead of reentering serverip and serverport
+        # con <server_ip> <server_port>
+        #       -> establishes connection and prints what it recieves
+        # add <server_ip> <server_port>
+        #       -> asks for some text to append to file, prints what it recieves
+        while True:
+            inp = input('>>')
+            cmd = inp.split(' ')
+
+            if cmd[0] == 'con':
+                if cmd[1] == 'last':
+                    server_ip, server_port = last
+                else:
+                    server_ip = cmd[1]
+                    server_port = int(cmd[2])
+                    last = (server_ip, server_port)
+                m.connections[(server_ip, server_port)] = Connection(self.ip, self.port, server_ip, server_port, send_socket=m.socket)
+                m.connections[(server_ip, server_port)]._send_syn()
+                print('sent syn')
+                connections[(server_ip, server_port)] = conn_queue.get()
+                print('We got em')
+                data = application_recv(connections[(server_ip, server_port)])
+                print(data)
+
+            elif cmd[0] == 'add':
+                if cmd[1] == 'last':
+                    server_ip, server_port = last
+                else:
+                    server_ip = cmd[1]
+                    server_port = int(cmd[2])
+                    last = (server_ip, server_port)
+                text = input('Enter some text to add: ')
+                application_send(connections[(server_ip, server_port)], bytes(text, 'utf-8'))
+                print('sent, now receiving: ')
+                data = application_recv(connections[(server_ip, server_port)])
+                print(data)
+
+            elif cmd[0] == 'dis':
+                if cmd[1] == 'last':
+                    server_ip, server_port = last
+                else:
+                    server_ip = cmd[1]
+                    server_port = int(cmd[2])
+                    last = (server_ip, server_port)
+                connections[(server_ip, server_port)].close()
+
+            else:
+                print('Sorry no comprende')
+
+
+if __name__ == '__main__':
+    port, file_dir = get_args(2)
+    ip = get_non_loopback_ip()
+    client = NiceClient(ip, int(port))
+    client.start()
+    
